55d14be65ef47af77eeb03cfedd2a63851f3fe19
Ahmet Tugur <atugur@migros.com.tr>
2022-11-18 23:51:10 +0300

    first commit

---
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/commands/BaseCommand.java
@@ -0,0 +1,15 @@
+package com.techbank.cqrs.core.commands;
+
+import com.techbank.cqrs.core.messages.Message;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper=true)
+public abstract class BaseCommand extends Message {
+    public BaseCommand(String id) {
+        super(id);
+    }
+}
