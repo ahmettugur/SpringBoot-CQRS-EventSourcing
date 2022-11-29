7d5c3592347d2b4712f2c91c19a667b6cd5c0761
ahmettugur <ahmet_tgr@hotmail.com>
2022-11-29 19:35:44 +0300

    added CloseAccountController

--- bank-account/account.common/src/main/java/com/techbank/account/common/events/AccountClosedEvent.java
+++ bank-account/account.common/src/main/java/com/techbank/account/common/events/AccountClosedEvent.java
@@ -1,12 +1,15 @@
 package com.techbank.account.common.events;
 
 import com.techbank.cqrs.core.events.BaseEvent;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
+@NoArgsConstructor
 public class AccountClosedEvent extends BaseEvent {
 
 }
